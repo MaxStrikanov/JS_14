e329edad04630c5673ff397e220bd3c087085b24
Max Strikanov <maxvell.88@mail.ru>
2020-08-11 19:38:44 +0300

    Второй урок

--- script/index.js
+++ script/index.js
@@ -1,10 +1,17 @@
-let money;
-let income;
-let addExpenses;
-let deposit;  
-let mission;  
-let period;
+const money = 150000, 
+      income = 'Дивиденды', 
+      addExpenses = 'Продукты, Комуналка, Ипотека, Машина, Развлечения',
+      deposit = true, 
+      mission = 5900000, 
+      period = 12;
 
-alert('Приветствую тебя, о мою юный падаван');
+let budgetDay = money / 30;
 
-console.log('Да прибудет с тобой сила!');
+console.log( typeof money );
+console.log( typeof income );
+console.log( typeof deposit );
+console.log( addExpenses.length );
+console.log( 'Период равен ' + period + ' месяцев и Цель заработать '+ mission + ' рублей' );
+console.log( 'Дневной бюджет: ' + budgetDay + ' рублей' );
+console.log( addExpenses.toLowerCase() );
+console.log( addExpenses.split( ", " ) );
