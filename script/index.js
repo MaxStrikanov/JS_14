29e58756b2a4a177b8ca21eb8b45246a1f0f530c
Max Strikanov <maxvell.88@mail.ru>
2020-08-11 19:35:21 +0300

    Второй урок

--- script/index.js
+++ script/index.js
@@ -1,9 +1,9 @@
-const money = 0, 
-      income = 0, 
-      addExpenses = 0, 
-      deposit = 0, 
-      mission = 0, 
-      period = 0;
+let money;
+let income;
+let addExpenses;
+let deposit;  
+let mission;  
+let period;
 
 alert('Приветствую тебя, о мою юный падаван');
 
