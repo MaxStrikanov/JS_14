982d849375e8611ef8bfb491a5758991fc5580df
Max Strikanov <maxvell.88@mail.ru>
2020-08-11 18:14:55 +0300

    Первый коммит

---
+++ script/index.js
@@ -0,0 +1,10 @@
+const money = 0, 
+      income = 0, 
+      addExpenses = 0, 
+      deposit = 0, 
+      mission = 0, 
+      period = 0;
+
+alert('Приветствую тебя, о мою юный падаван');
+
+console.log('Да прибудет с тобой сила!');
